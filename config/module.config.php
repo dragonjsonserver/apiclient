a4110ae14c4fde2a15022be314e44616d52f5032
Christoph Herrmann <developer@dragonprojects.de>
2014-03-30 10:07:40 +0200

    Erweiterung für den DragonJsonServer zur Erstellung eines API Clients aus der SMD

---
+++ config/module.config.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @link http://dragonjsonserver.de/
+ * @copyright Copyright (c) 2012-2014 DragonProjects (http://dragonprojects.de/)
+ * @license http://license.dragonprojects.de/dragonjsonserver.txt New BSD License
+ * @author Christoph Herrmann <developer@dragonprojects.de>
+ * @package DragonJsonServerApiclient
+ */
+
+/**
+ * @return array
+ */
+return [
+    'dragonjsonserverapiclient' => [
+        'namespace' => 'Emailaddress',
+        'method'    => 'loginEmailaddress',
+    ],
+    'router' => [
+        'routes' => [
+            'home' => [
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => [
+                    'route' => '/',
+                    'defaults' => [
+                        'controller' => 'DragonJsonServerApiclient\Controller\Apiclient',
+                        'action' => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'controllers' => [
+        'invokables' => [
+            'DragonJsonServerApiclient\Controller\Apiclient' => 'DragonJsonServerApiclient\Controller\ApiclientController'
+        ],
+    ],
+    'view_manager' => [
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
+    ],
+];
---
+++ src/DragonJsonServerApiclient/Controller/ApiclientController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @link http://dragonjsonserver.de/
+ * @copyright Copyright (c) 2012-2014 DragonProjects (http://dragonprojects.de/)
+ * @license http://license.dragonprojects.de/dragonjsonserver.txt New BSD License
+ * @author Christoph Herrmann <developer@dragonprojects.de>
+ * @package DragonJsonServerApiclient
+ */
+
+namespace DragonJsonServerApiclient\Controller;
+
+class ApiclientController extends \Zend\Mvc\Controller\AbstractActionController
+{
+    public function indexAction()
+    {
+        $config = $this->getServiceLocator()->get('Config');
+    	return [
+            'application' => $config['dragonjsonserver']['application'],
+            'server'      => $config['dragonjsonserver']['server'],
+            'apiclient'   => $config['dragonjsonserverapiclient'],
+        ];
+    }
+}
